ce6022658108f9cc85ac415b99ce46c09a553751
Benoit Boucounaud <b.boucounaud@nodya-group.com>
2021-07-15 12:24:32 +0200

    Home comp + first comp

--- base-site/src/app/app.component.ts
+++ base-site/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root', // balise name
+    templateUrl: './app.component.html', // html road
+    styleUrls: ['./app.component.scss'] // css road
 })
 export class AppComponent {
-  title = 'base-site';
+    title = 'Classic app to keep basics close';
 }
--- base-site/src/app/app.module.ts
+++ base-site/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FirstComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [HomeComponent] // home page route
 })
 export class AppModule { }
