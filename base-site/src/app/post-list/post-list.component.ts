9aee2205f81b2efe8a7c71b49b122c8c538662ff
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2021-07-21 19:50:43 +0200

    post list got list

--- base-site/src/app/post-list/post-list.component.ts
+++ base-site/src/app/post-list/post-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+    posts = []
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngOnInit(): void {
+    }
+
+    // TODO : make a getter who catch each post from posts.json
+    // it will catch each post, transform it in a Post class and add it in posts
 }
