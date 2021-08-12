d6c1c58bfc13cbccb2d5c3fa1de25d44b735fe8f
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2021-08-12 16:54:41 +0200

    rebuild home ts

--- base-site/src/app/home/home.component.ts
+++ base-site/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ApparatusService } from '../services/apparatus.service';
 
 @Component({
     selector: 'app-home',
@@ -10,43 +8,11 @@ import { ApparatusService } from '../services/apparatus.service';
 export class HomeComponent implements OnInit {
 
     title = 'Classic app to keep basics close';
-    isAuth = false;
-    lastUpdate = new Date()!
-    apparatuses: any = {}
 
-
-    // We emulate catching data from server
-    lastUpdateAsync = new Promise((resolve, reject) => {
-        const date = new Date();
-        setTimeout(
-            () => {
-                resolve(date);
-            }, 2000
-        );
-    });
-
-    constructor(private router: Router, private apparatusService: ApparatusService) {
-        // set 4 sec to make isAuth = true
-        setTimeout(
-            () => {
-                this.isAuth = true;
-            }, 4000
-        );
+    constructor() {
     }
 
     ngOnInit(): void {
-        // define apparatus
-        this.apparatuses = this.apparatusService.apparatuses
     }
-
-    onTurnOn() {
-        this.apparatusService.switchOnAll()
-    }
-    onTurnOff() {
-        if (confirm('Are you sure you want to turn off all your apparatuses? ')) {
-            this.apparatusService.switchOffAll()
-        }
-    }
-
 
 }
