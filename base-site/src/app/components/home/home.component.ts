d9d6629e5764d68ab761774d2b5de1f0d057e1d3
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2021-08-13 13:57:12 +0200

    create observable

--- base-site/src/app/components/home/home.component.ts
+++ base-site/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
     selector: 'app-home',
@@ -8,11 +11,34 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
     title = 'Classic app to keep basics close';
+    seconds: number = 0;
+    counterSubscription: Subscription = new Subscription;
 
     constructor() {
     }
 
     ngOnInit(): void {
+        const counter = Observable.interval(1000);
+
+        this.counterSubscription = counter.subscribe(
+            // if observable emit new data
+            (value) => {
+                this.seconds = value;
+            },
+            // if ko
+            (error) => {
+                console.log('Uh-oh, an error occurred! : ' + error);
+            },
+            // if observable ends
+            () => {
+                console.log('Observable complete!');
+            }
+        );
+    }
+
+    // activated when a component is destroyed
+    ngOnDestroy() {
+        this.counterSubscription.unsubscribe();
     }
 
 }
