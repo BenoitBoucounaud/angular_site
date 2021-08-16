1683c940216311a90e67d5873b864f02cd109fd4
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2021-08-16 14:57:14 +0200

    add validator to formBuilder

--- base-site/src/app/components/new-user/new-user.component.ts
+++ base-site/src/app/components/new-user/new-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/User.model';
 import { UserService } from 'src/app/services/user.service';
@@ -24,10 +24,10 @@ export class NewUserComponent implements OnInit {
 
     initForm() {
         this.userForm = this.formBuilder.group({
-            firstName: '',
-            lastName: '',
-            email: '',
-            drinkPreference: ''
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            drinkPreference: ['', Validators.required]
         });
     }
 
