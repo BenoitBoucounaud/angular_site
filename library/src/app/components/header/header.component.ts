b8e65fcaa62c3e6226e5594cf71839020fe94b52
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2021-08-18 15:16:31 +0200

    add isAuth in header

--- library/src/app/components/header/header.component.ts
+++ library/src/app/components/header/header.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    isAuth: boolean = false;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private authService: AuthService,
+        public afAuth: AngularFireAuth,
+    ) { }
+
+    ngOnInit() {
+        this.afAuth.onAuthStateChanged(
+            (user) => {
+                if (user) {
+                    this.isAuth = true;
+                } else {
+                    this.isAuth = false;
+                }
+            }
+        );
+    }
+
+    onSignOut() {
+        this.authService.signOutUser();
+    }
 
 }
