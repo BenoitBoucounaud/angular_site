65ecba12eda7579a39e4f18814c2e371d6629d57
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2021-08-18 12:48:38 +0200

    add auth guard to books routes

--- library/src/app/app-routing.module.ts
+++ library/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { SignupComponent } from './components/auth/signup/signup.component';
 import { BookFormComponent } from './components/book-list/book-form/book-form.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { SingleBookComponent } from './components/book-list/single-book/single-book.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
     { path: 'auth/signup', component: SignupComponent },
     { path: 'auth/signin', component: SigninComponent },
-    { path: 'books', component: BookListComponent },
-    { path: 'books/new', component: BookFormComponent },
-    { path: 'books/view/:id', component: SingleBookComponent }
+    { path: 'books', canActivate: [AuthGuardService], component: BookListComponent },
+    { path: 'books/new', canActivate: [AuthGuardService], component: BookFormComponent },
+    { path: 'books/view/:id', canActivate: [AuthGuardService], component: SingleBookComponent }
 ];
 
 @NgModule({
