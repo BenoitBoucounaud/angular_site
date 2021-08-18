1b22b8d6b51fbd69f648e185bedd3142add2c90d
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2021-08-18 11:40:19 +0200

    add routes

--- library/src/app/app-routing.module.ts
+++ library/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { BookFormComponent } from './components/book-list/book-form/book-form.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { SingleBookComponent } from './components/book-list/single-book/single-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'auth/signup', component: SignupComponent },
+    { path: 'auth/signin', component: SigninComponent },
+    { path: 'books', component: BookListComponent },
+    { path: 'books/new', component: BookFormComponent },
+    { path: 'books/view/:id', component: SingleBookComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
